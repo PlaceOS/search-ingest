3ac4b1c88f57274803c19d98f36bc3429e094c1b
Caspian Baska <caspianbaska@gmail.com>
2019-05-10 11:04:11 +1000

    (api) remove backfill/reindex for specific tables

--- src/api.cr
+++ src/api.cr
@@ -23,23 +23,8 @@ class RubberSoul::API < ActionController::Base
     table_manager.backfill_all if backfill
   end
 
-  # Allow specific tables to be reindexed
-  # Backfills by default
-  #   ensure all dependencies reindexed?
-  post "/reindex/:table", :reindex_table do
-    # backfill = params[:backfill]? || true
-    # reindex(params[:table], backfill)
-    head :not_implemented
-  end
-
   # Backfill all tables
   post "/backfill", :backfill_all do
     table_manager.backfill_all
-  end
-
-  # Backfill specific table,
-  #   as in reindex, ensure all dependencies backfilled?
-  post "/backfill/:table", :backfill_table do
-    head :not_implemented
   end
 end
