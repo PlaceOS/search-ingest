01eda9a3d7c911602d27b5c63c26db46ff8066c6
Stephen von Takach <steve@acaprojects.com>
2020-02-17 16:46:09 +1100

    fix(constants): improved version extraction

--- src/constants.cr
+++ src/constants.cr
@@ -2,5 +2,5 @@ module RubberSoul
   RETHINK_DATABASE = ENV["RETHINKDB_DB"]? || "test"
   APP_NAME         = "rubber-soul"
   # calculate version at compile time
-  VERSION          = {{ system("shards version").stringify.strip.downcase }}
+  VERSION          = {{ `shards version "#{__DIR__}"`.chomp.stringify.downcase }}
 end
