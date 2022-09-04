fbf3c50abfb540c978125eeed489505bbc10185f
Stephen von Takach <steve@place.technology>
2022-09-04 19:05:40 +1000

    feat: add support for ARM64 and update libs

--- spec/api_spec.cr
+++ spec/api_spec.cr
@@ -1,19 +1,16 @@
 require "./helper"
 
 describe SearchIngest::Api do
-  # ==============
-  # Test Responses
-  # ==============
-  with_server do
-    it "health checks" do
-      result = curl("GET", SearchIngest::Api::NAMESPACE[0])
-      result.success?.should be_true
-    end
+  client = AC::SpecHelper.client
 
-    it "should check version" do
-      result = curl("GET", "/api/search-ingest/v1/version")
-      result.status_code.should eq 200
-      PlaceOS::Model::Version.from_json(result.body).service.should eq "search-ingest"
-    end
+  it "health checks" do
+    result = client.get SearchIngest::Api::NAMESPACE[0]
+    result.success?.should be_true
+  end
+
+  it "should check version" do
+    result = client.get "/api/search-ingest/v1/version"
+    result.status_code.should eq 200
+    PlaceOS::Model::Version.from_json(result.body).service.should eq "search-ingest"
   end
 end
